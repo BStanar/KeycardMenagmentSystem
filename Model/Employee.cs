5a00ff99a3853ea56ede38e4fe904fe879c759a4
BStanar <bstanar96@gmail.com>
2024-02-19 12:30:50 +0100

    Sql kod i modeli
    Sql kod i modeli

---
+++ Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.Model
+{
+    internal class Employee : Person
+    {
+        private string _department;
+        
+        public string Department
+        {
+            get { return _department; }
+            set { _department = value; }
+        }
+        public Employee(int id, string lastName, string email, string firstName, string password, string username, DateTime startOfEmployment) : base(id, lastName, email, firstName, password, username, startOfEmployment)
+        {
+
+        }
+    }
+}
---
+++ Model/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.Model
+{
+    internal class Person
+    {
+        private int _id;
+        private string _lastName;
+        private string _email;
+        private string _firstName;
+        private string _password;
+        private string _username;
+        private DateTime _startOfEmployment;
+
+
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string Lastname { get { return _lastName; } set { _lastName = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        public int ID { get { return _id; } }
+        public string Password { get { return _password; } }
+        public string Username { get { return _username; } }
+
+        public DateTime StartOfEmployment
+        {
+            get { return _startOfEmployment; }
+        }
+
+        /*Konstruktor za menadzera*/
+        public Person(int id, string firstName, string lastName, string email, string password, string username) {
+            FirstName = firstName;
+            Lastname = lastName;
+            Email = email;
+            this._id = id;
+            this._password = password;
+            this._username = username;
+        }
+        /*Konstruktor za emplyee*/
+        public Person(int id, string firstName, string lastName, string email, string password, string username, DateTime startOfEmployment)
+        {
+            FirstName = firstName;
+            Lastname = lastName;
+            Email = email;
+            this._id = id;
+            this._password = password;
+            this._username = username;
+            this._startOfEmployment = startOfEmployment;
+            
+        }
+    }
+}
