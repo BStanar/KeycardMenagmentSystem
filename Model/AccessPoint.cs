4c5f4aed52b46251a0b3a934e9c64c6b75b20f94
BStanar <bstanar96@gmail.com>
2024-02-19 14:26:55 +0100

    Acces point logs

--- Model/AccessPoint.cs
+++ Model/AccessPoint.cs
@@ -10,12 +10,18 @@ namespace KeycardMenagmentSystem.Model
     {
         private int _accesPointID;
         private string _name;
+        private List<Log> _accessPointLog=new List<Log>();
         
         public int AccesPointID {  get { return _accesPointID; } set { _accesPointID = value; } }
         public string Name { get { return _name; } set { _name = value; } } 
         public AccessPoint(int accesPointId, string name ) {
             AccesPointID = accesPointId;
             Name = name;
+        }
+
+        public void AddAccessPointLog() {
+            Log log = new Log();
+            _accessPointLog.Add(new Log());
         }
     }
 }
