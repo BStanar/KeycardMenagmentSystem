d3f18a6974ece8edb3b8ae658df49b4311dd5081
BStanar <bstanar96@gmail.com>
2024-02-19 13:09:02 +0100

    Made view viewmodel foleders. Added some data to sql table

--- View/KeyCard.cs
+++ View/KeyCard.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeycardMenagmentSystem.Model
+namespace KeycardMenagmentSystem.View
 {
-    internal class AccessPoint
+    internal class KeyCard
     {
-
     }
 }
