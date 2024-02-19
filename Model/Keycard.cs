2b578567ba80207342f06e61bbd1784552a5c934
BStanar <bstanar96@gmail.com>
2024-02-19 12:30:50 +0100

    Sql kod i modeli
    Sql kod i modeli

---
+++ Model/Keycard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.Model
+{
+    internal class Keycard
+    {
+        private int _keycardID;
+        private string _serialCode;
+        
+        private Employee _employee;
+
+        public int KeycardID {  get { return _keycardID; } set { _keycardID = value; } }
+        public string SerialCode { get { return _serialCode;} set { _serialCode = value; } }
+        public Employee Employee { get { return _employee; } }
+    }
+}
