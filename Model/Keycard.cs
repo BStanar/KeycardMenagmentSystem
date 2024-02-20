c823b65c808baf513856c0857bba911fb0e979a5
elina-1201 <elina-1201@yandex.com>
2024-02-20 15:14:25 +0100

    Add activated field in Keycard Model and database.

--- Model/Keycard.cs
+++ Model/Keycard.cs
@@ -11,14 +11,17 @@ namespace KeycardMenagmentSystem.Model
         private int _keycardID;
         private string _serialCode;
         private int _userId;
+        private bool _activated
 
        
         public int KeycardID {  get { return _keycardID; } set { _keycardID = value; } }
         public string SerialCode { get { return _serialCode;} set { _serialCode = value; } }
         public int UserId { get{ return _userId; } set { _userId = value; } }
+        public bool Activated { get { return _activated; } set { _activated = value; } }
 
         public Keycard(int keycardID, string serialCode, int idUser)
         {
+            _activated = false;
             KeycardID = keycardID;
             SerialCode = serialCode;
             _userId = idUser;
