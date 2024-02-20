7d5b573dbb1d8497d31c26b0e6c1b3fa19ecd961
elina-1201 <elina-1201@yandex.com>
2024-02-20 13:45:27 +0100

    Add ViewModels connected to AccessPoint.

---
+++ ViewModel/AccessPointListingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KeycardMenagmentSystem.ViewModel
+{
+    public class AccessPointListingViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<AccessPointViewModel> _accessPoints;
+        public IEnumerable<AccessPointViewModel> AccessPoints => _accessPoints;
+
+        public AccessPointListingViewModel()
+        {
+            _accessPoints = new ObservableCollection<AccessPointViewModel>();
+        }
+    }
+}
---
+++ ViewModel/AccessPointViewModel.cs
@@ -0,0 +1,22 @@
+﻿using KeycardMenagmentSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.ViewModel
+{
+    public class AccessPointViewModel : ViewModelBase
+    {
+        private readonly AccessPoint? _accessPoint;
+
+        public string AccessPointID => _accessPoint.AccesPointID.ToString();
+        public string Name => _accessPoint.Name;
+
+        public AccessPointViewModel(AccessPoint accessPoint)
+        {
+            _accessPoint = accessPoint;
+        }
+    }
+}
