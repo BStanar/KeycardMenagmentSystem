ca063b9538b0f54ca4f07becff1a70c02903267f
BStanar <bstanar96@gmail.com>
2024-02-21 14:52:19 +0100

    Get data from AccesLogs, Buttons left

---
+++ Services/IGetAccessLogs.cs
@@ -0,0 +1,14 @@
+﻿using KeycardMenagmentSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.Services
+{
+    internal interface IGetAccessLogs
+    {
+        public Task<List<AccessLog>> GetAccesLogs();
+    }
+}
---
+++ ViewModel/LogsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using KeycardMenagmentSystem.Model;
+using KeycardMenagmentSystem.View;
+using KeycardMenagmentSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeycardMenagmentSystem.ViewModel
+{
+    internal class LogsViewModel
+    {
+        private readonly ObservableCollection<AccessLog> _accessLog;
+        public IEnumerable<AccessLog> AccessLogs => _accessLog;
+
+        private readonly IGetAccessLogs _getAccessLogsService;
+
+        public LogsViewModel(IGetAccessLogs getAccessLogsService)
+        {
+            _getAccessLogsService = getAccessLogsService;
+            _accessLog = new ObservableCollection<AccessLog>();
+
+            LoadAccessLogs();
+        }
+
+        private async void LoadAccessLogs()
+        {
+
+            var accessLogs = await _getAccessLogsService.GetAccesLogs();
+            _accessLog.Clear();
+            foreach (var accesslog in accessLogs)
+            {
+                _accessLog.Add(accesslog);
+            }
+
+        }
+    }
+}
